47dbfd975c2e8e001cc293e73579b6bb2a2b10ae
Maxime Mouchet <max@maxmouchet.com>
2020-05-16 16:13:43 +0200

    Add TracerouteMapASNTransformer/TracerouteMapIXTransformer

--- fetchmesh/transformers/record.py
+++ fetchmesh/transformers/record.py
@@ -1,7 +1,11 @@
 from abc import ABC, abstractmethod
+from collections import defaultdict
 from dataclasses import dataclass
 from functools import lru_cache
 from ipaddress import ip_address
+from typing import Optional, Tuple
+
+from radix import Radix
 
 
 class RecordTransformer(ABC):
@@ -16,6 +20,46 @@ class RecordTransformer(ABC):
 class PingMinimumTransformer(RecordTransformer):
     def transform(self, record: dict) -> dict:
         return {"timestamp": record["timestamp"], "min": record["min"]}
+
+
+# TODO: More generic TracerouteMapAddrTransformer?
+# Takes key and fn(key) -> val as input.
+
+
+@dataclass
+class TracerouteMapASNTransformer(RecordTransformer):
+    tree: Radix
+
+    def transform(self, record: dict) -> dict:
+        new_record = record.copy()
+        for i, hop in enumerate(record.get("result", [])):
+            for j, reply in enumerate(hop.get("result", [])):
+                try:
+                    # TODO: Handle multiple origins
+                    # TODO: Cache lookups
+                    asn = self.tree.search_best(reply["from"]).data["origins"][0]
+                    new_record["result"][i]["result"][j]["asn"] = asn
+                except (AttributeError, KeyError):
+                    new_record["result"][i]["result"][j]["asn"] = None
+        return new_record
+
+
+@dataclass
+class TracerouteMapIXTransformer(RecordTransformer):
+    tree: Radix
+
+    def transform(self, record: dict) -> dict:
+        new_record = record.copy()
+        for i, hop in enumerate(record.get("result", [])):
+            for j, reply in enumerate(hop.get("result", [])):
+                try:
+                    # TODO: Handle multiple origins
+                    # TODO: Cache lookups
+                    ix = self.tree.search_best(reply["from"]).data["ix"].name
+                    new_record["result"][i]["result"][j]["ix"] = ix
+                except (AttributeError, KeyError):
+                    new_record["result"][i]["result"][j]["ix"] = None
+        return new_record
 
 
 @dataclass
@@ -49,6 +93,11 @@ class TracerouteFlatIPTransformer(RecordTransformer):
         - fd00::/8
     """
 
+    extras_fields: Tuple[str, ...] = tuple()
+    """
+    List of additional response fields to include (e.g. `asn`, `ix` ...)
+    """
+
     @staticmethod
     @lru_cache(maxsize=65536)
     def is_private(addr: str) -> bool:
@@ -56,8 +105,10 @@ class TracerouteFlatIPTransformer(RecordTransformer):
 
     def transform(self, record: dict) -> dict:
         hops = []
+        extras = defaultdict(list)  # type: ignore
         for hop in record.get("result", []):
             addrs = []  # type: ignore
+            extras_ = defaultdict(list)  # type: ignore
             for reply in hop.get("result", []):
                 # Sometimes results contains an empty object {}
                 if not reply:
@@ -70,8 +121,13 @@ class TracerouteFlatIPTransformer(RecordTransformer):
                 if self.drop_private and addr and self.is_private(addr):
                     addr = None
                 addrs.append(addr)
+                for field in self.extras_fields:
+                    extras_[field].append(reply.get(field))
             if self.as_set:
                 addrs = set(addrs)  # type: ignore
+                extras_ = {k: set(v) for k, v in extras_.items()}  # type: ignore
+            for field in self.extras_fields:
+                extras[field].append(extras_.get(field, {}))
             hops.append(addrs)
 
         return {
@@ -81,4 +137,5 @@ class TracerouteFlatIPTransformer(RecordTransformer):
             "dst_addr": record["dst_addr"],
             "paris_id": record["paris_id"],
             "hops": hops,
+            **extras,
         }
