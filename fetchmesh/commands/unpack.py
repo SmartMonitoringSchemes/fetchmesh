03ffbdd04c03673775ad1e371cec2ea66af1f56a
Maxime Mouchet <max@maxmouchet.com>
2020-05-16 18:22:59 +0200

    unpack: parallel

--- fetchmesh/commands/unpack.py
+++ fetchmesh/commands/unpack.py
@@ -1,3 +1,5 @@
+from multiprocessing import Pool
+
 import click
 from tqdm import tqdm
 
@@ -6,6 +8,27 @@ from ..ext import DateParseParamType, EnumChoice, PathParamType, bprint, format_
 from ..io import AtlasRecordsReader, AtlasRecordsWriter
 from ..meta import AtlasResultsMeta
 from ..utils import groupby_stream
+
+
+class UnpackWorker:
+    def __init__(self, src, dst):
+        self.src = src
+        self.dst = dst
+
+    def do(self, metas):
+        key = lambda x: (x["msm_id"], x["prb_id"])
+        for meta in sorted(metas, key=lambda x: x.start_date):
+            file = self.src.joinpath(meta.filename())
+            with AtlasRecordsReader(file) as r:
+                # TODO: Cleanup this ! :-)
+                r = filter(lambda x: x, r)
+                for pair, records in groupby_stream(r, key, 10 ** 6):
+                    name = "{}_{}.ndjson".format(*pair)
+                    if meta.compressed:
+                        name += ".zst"
+                    file = self.dst.joinpath(name)
+                    with AtlasRecordsWriter(file, compression=False, append=True) as w:
+                        w.writeall(records)
 
 
 @click.command()
@@ -27,22 +50,28 @@ from ..utils import groupby_stream
 @click.option(
     "--stop-date", type=DateParseParamType(), help="Results stop date",
 )
-@click.argument("source", required=True, type=PathParamType())
-@click.argument("dest", default=".", type=PathParamType())
-# TODO: Implement parallel processing
-# => Hard because we don't know pairs in advance.
+@click.option(
+    "--jobs",
+    default=1,
+    show_default=True,
+    metavar="N",
+    type=click.IntRange(min=1),
+    help="Number of parallel jobs to run",
+)
+@click.argument("src", required=True, type=PathParamType())
+@click.argument("dst", default=".", type=PathParamType())
 def unpack(**args):
     """
     Split measurement results by pairs.
     """
     bprint("Args", format_args(args))
 
-    args["dest"].mkdir(exist_ok=True, parents=True)
+    args["dst"].mkdir(exist_ok=True, parents=True)
 
     # (1) Index meta files
     # index[af][type][msm_id][date]
     index = {af: {t: {} for t in MeasurementType} for af in MeasurementAF}
-    files = args["source"].glob("*.ndjson*")
+    files = args["src"].glob("*.ndjson*")
     for file in files:
         meta = AtlasResultsMeta.from_filename(str(file))
         index[meta.af][meta.type].setdefault(meta.msm_id, [])
@@ -67,19 +96,8 @@ def unpack(**args):
         index[msm_id] = metas
 
     # TODO: Cleanup dir before
-    for (msm_id, metas) in tqdm(index.items()):
-        for meta in tqdm(metas, leave=False):
-            key = lambda x: (x["msm_id"], x["prb_id"])
-            file = args["source"].joinpath(meta.filename())
-            with AtlasRecordsReader(file) as r:
-                # TODO: Cleanup this ! :-)
-                r = filter(lambda x: x, r)
-                for pair, records in groupby_stream(r, key, 10 ** 6):
-                    name = "{}_{}.ndjson".format(*pair)
-                    if meta.compressed:
-                        name += ".zst"
-                    file = args["dest"].joinpath(name)
-                    with AtlasRecordsWriter(
-                        file, compression=meta.compressed, append=True
-                    ) as w:
-                        w.writeall(records)
+    worker = UnpackWorker(args["src"], args["dst"])
+    # NOTE: // processing is safe here since we process metadata sequentially
+    # for a given measurement ID, and file names are $(msm_id)_$(prb_id).
+    with Pool(args["jobs"]) as p:
+        list(tqdm(p.imap(worker.do, index.values()), total=len(index)))
