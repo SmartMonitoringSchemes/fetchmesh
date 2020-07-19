6cfd9772f8c7b2ff8a20c3fb2ffc4de9479b6b04
Maxime Mouchet <max@maxmouchet.com>
2020-07-19 21:38:43 +0200

    Replace tableformatter with rich

--- fetchmesh/commands/describe.py
+++ fetchmesh/commands/describe.py
@@ -2,9 +2,12 @@
 from collections import defaultdict
 
 import click
-import tableformatter as tf
 from mtoolbox.click import ParsedDate
 from mtoolbox.itertools import countby
+from rich import box
+from rich.console import Console
+from rich.table import Table
+from rich.text import Text
 
 from ..atlas import MeasurementAF, MeasurementType
 from ..ext import bprint
@@ -34,6 +37,7 @@ def describe(date):
     Anchoring mesh overview.
     """
 
+    console = Console()
     mesh = AnchoringMesh.from_api().filter(MeasurementDateFilter.running(date, date))
 
     # TODO: Number of distinct pairs counted, vs theoretical number
@@ -55,38 +59,79 @@ def describe(date):
         "Number of origin-destination pairs (noself,half)", f"{pairs_obs}/{pairs_exp}"
     )
 
+    print()
+
     # Intra-region count
     counts = countby(mesh.anchors, lambda x: x.country.main_region)
-    rows = [
-        ["Anchors"] + list(counts.values()),
-        ["Pairs"] + [expected_pairs(count, False, False) for count in counts.values()],
-    ]
-    cols = [""] + list(counts.keys())
-    print(tf.generate_table(rows, cols))
+    counts = sorted(counts.items())
+
+    table = Table(
+        title="Intra-region counts", box=box.MINIMAL_HEAVY_HEAD, show_lines=True
+    )
+
+    table.add_column("")
+    for x in counts:
+        table.add_column(x[0])
+
+    table.add_row(Text("Anchors", style="bold"), *[str(x[1]) for x in counts])
+    table.add_row(
+        Text("Pairs", style="bold"),
+        *[str(expected_pairs(x[1], False, False)) for x in counts],
+    )
+
+    console.print(table)
 
     # Inter-region count
     counts = countby(mesh.anchors, lambda x: x.country.main_region)
-    rows = [[k] + [a * b for b in counts.values()] for k, a in counts.items()]
-    cols = [""] + list(counts.keys())
-    print(tf.generate_table(rows, cols))
+    counts = sorted(counts.items())
+
+    table = Table(
+        title="Inter-region counts", box=box.MINIMAL_HEAVY_HEAD, show_lines=True
+    )
+
+    table.add_column("")
+    for x in counts:
+        table.add_column(x[0])
+
+    for k, a in counts:
+        table.add_row(Text(k, style="bold"), *[str(a * b) for _, b in counts])
+
+    console.print(table)
 
     # Top AS counts
     counts = countby(mesh.anchors, lambda x: x.as_v4)
     counts = dict(sorted(counts.items(), key=lambda x: x[1], reverse=True)[:5])
-    rows = [
-        ["Anchors"] + list(counts.values()),
-        ["Pairs"] + [expected_pairs(count, False, False) for count in counts.values()],
-    ]
-    cols = [""] + list(counts.keys())
-    print(tf.generate_table(rows, cols))
+
+    table = Table(title="Top AS counts", box=box.MINIMAL_HEAVY_HEAD, show_lines=True)
+
+    table.add_column("")
+    for x in counts:
+        table.add_column(str(x))
+
+    table.add_row(
+        Text("Anchors", style="bold"), *[str(count) for count in counts.values()],
+    )
+    table.add_row(
+        Text("Pairs", style="bold"),
+        *[str(expected_pairs(count, False, False)) for count in counts.values()],
+    )
+
+    console.print(table)
 
     # Types counts
     counts = defaultdict(lambda: defaultdict(int))
     for m in mesh.measurements:
         counts[m.af][m.type] += 1
 
-    rows = [
-        [af.name] + [counts[af][t] for t in MeasurementType] for af in MeasurementAF
-    ]
-    cols = [""] + [x.name for x in MeasurementType]
-    print(tf.generate_table(rows, cols))
+    table = Table(title="Types counts", box=box.MINIMAL_HEAVY_HEAD, show_lines=True)
+
+    table.add_column("")
+    for x in MeasurementType:
+        table.add_column(x.name)
+
+    for af in MeasurementAF:
+        table.add_row(
+            Text(af.name, style="bold"), *[str(counts[af][t]) for t in MeasurementType]
+        )
+
+    console.print(table)
