901ee062d0a680a1e770b4cb60ebf27911795a9f
Maxime Mouchet <max@maxmouchet.com>
2020-09-07 22:46:34 +0200

    asn: test ASNames.from_url

--- tests/asn/test_names.py
+++ tests/asn/test_names.py
@@ -1,9 +1,17 @@
 from pathlib import Path
 
+import pytest
+
 from fetchmesh.asn import ASNames
 
 
-def test_asnames():
+def test_from_file():
     f = Path(__file__).parent / "data" / "asn.txt"
     names = ASNames.from_file(f)
+    assert names.mapping[288] == "ESA European Space Agency (ESA), EU"
+
+
+@pytest.mark.online
+def test_from_url():
+    names = ASNames.from_url()
     assert names.mapping[288] == "ESA European Space Agency (ESA), EU"
