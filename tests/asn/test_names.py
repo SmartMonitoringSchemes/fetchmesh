5497f53fa229d913c0fc4650f5dfd2d92a71fc42
Maxime Mouchet <max@maxmouchet.com>
2020-09-07 22:39:13 +0200

    asn: test ASNDB and ASNames

---
+++ tests/asn/test_names.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+from fetchmesh.asn import ASNames
+
+
+def test_asnames():
+    f = Path(__file__).parent.joinpath("asn.txt")
+    names = ASNames.from_file(f)
+    assert names.mapping[288] == "ESA European Space Agency (ESA), EU"
