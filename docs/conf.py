cf730864453b8dcb46d68d4fd3a24176775bf710
Maxime Mouchet <max@maxmouchet.com>
2020-09-14 22:23:52 +0200

    docs: cleanup

--- docs/conf.py
+++ docs/conf.py
@@ -53,15 +53,6 @@ autodoc_member_order = "groupwise"
 html_theme = "alabaster"
 html_logo = "logo.png"
 
-html_theme_options = {
-    "fixed_sidebar": True,
-    "github_user": "maxmouchet",
-    "github_repo": "fetchmesh",
-    "github_type": "star",
-    "page_width": "1240px",
-    "sidebar_width": "260px",
-}
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
